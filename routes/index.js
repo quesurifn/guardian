cb3557c7aefd0e3b06544583aa82aad5faeb9a05
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-10 15:11:54 -0500

    remove shopify

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const Shopify = require('shopify-api-node');
 const moment = require('moment');
 const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 const pg = require('pg');
