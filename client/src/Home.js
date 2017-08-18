5e719e7589e195840f2f6f895e8ddff5b15f5b0d
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-18 10:37:01 -0500

    Photos

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import cardOne from './images/leak.png'
+import cardTwo from './images/faceb.png'
 
 
 import {connect} from 'react-redux'
@@ -28,22 +29,36 @@ export class Home extends Component {
           <div className='col-md-6'>
             <div className='cardOne'>
 
-              <img src={cardOne} alt='cardOne' />
+              <div className="cardOneImg"></div>
               <div className='cardOneBottom'> 
                 <h2>Toolless Installation</h2>
 
                 <p>No plumbing. No tools. Guardian was designed so that anyone can install it in minutes.</p>
 
                 <div className='watch'>
-                  <span>WATCH VIDEO</span>
+                  <span>WATCH VIDEO<i className="fa fa-long-arrow-right" aria-hidden="true"></i></span>
                 </div>
               </div> 
             </div>
           </div>
           <div className='col-md-6'>
-            
+            <div className='cardTwo'>
+
+              <div className="cardTwoImg"></div>
+              <div className='cardTwoBottom'> 
+                <h2>RF Based Communication</h2>
+
+                <p>Learn more about our ambitious move to provide long range RF based system that communicates directly over Wi-Fi.</p>
+
+                <div className='watch'>
+                  <span>LEARN MORE<i className="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                </div>
+              </div> 
+            </div>
           </div>
         </div>
+        <div className='row banner'>
+       </div>
       </div>
 
       
