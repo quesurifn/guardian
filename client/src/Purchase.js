6f8ebcfe59bf849f46213973d2a81e5bcf855c08
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-26 07:04:56 -0500

    Checkpoint

--- client/src/Purchase.js
+++ client/src/Purchase.js
@@ -3,6 +3,10 @@ import './App.css';
 import {Button, Row, Col} from 'react-materialize'
 import {Link} from 'react-router-dom'
 
+import hero from './images/hero.png'
+import duck from './images/duckie.jpg'
+import red from './images/btn-get-guardian-red.png'
+
 
 
 
@@ -11,6 +15,9 @@ export class Purchase extends Component {
         document.querySelector('nav').style.backgroundColor = 'transparent'
         document.querySelector('nav').style.position = 'absolute'
         document.querySelector('nav').style.boxShadow = 'none'
+        document.querySelector('.nav-wrapper').style.backgroundColor = 'transparent'
+        document.querySelector('.navbar-fixed').style.position = 'absolute'
+        document.querySelector('.navbar-fixed').style.width = '100%'
         
         
         
@@ -29,7 +36,35 @@ export class Purchase extends Component {
     return (
       <div>
         <Row>
-lol
+            <Col s={12} className='gBlue'>
+                <img src={hero} alt='yoyoyo' className='purchaseHero'/>
+            </Col>
+        </Row>
+        <Row>
+            <Col s={12} className='purchaseTwo'>
+               <div className='anton'>
+                   <div className='text'>
+                        <h2>one less thing...</h2>
+                        <h3>to worry about</h3>
+                        <img src={red} alt='red button' />
+                    </div>
+               </div>
+
+
+            </Col>
+        </Row>
+        <Row>
+            <Col s={12} className='purchaseThree'>
+                <p>Americans are more likely to experience water damage than fire.</p>
+            </Col>
+        </Row>
+        <Row>
+            <Col s={12} className='purchaseFour'>
+                <div>
+                
+
+                </div>
+            </Col>
         </Row>
       </div>
     );
