aa63d9b9365756ffa740fcac72f1195b3a980f98
Kyle <kyle.c.r.fahey@gmail.com>
2017-09-05 11:34:49 -0500

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import {Home} from './Home'
+import {AppComponent} from './AppComponent'
 
 export const App = () => (
   <div>
     <main>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/app" component={AppComponent} />
+        <Route path='*' component={Home} />
       </Switch>
     </main>
   </div>
