acd0431fc9db7a8bdceb7fe7c4da6c001ff17777
Kyle <kyle.c.r.fahey@gmail.com>
2017-09-05 11:50:25 -0500

    All

--- client/src/modules/module.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
-
-import { reducer } from '../reducers/reducer'
-
-export default combineReducers({
-  reducer,
-  routing: routerReducer
-})
