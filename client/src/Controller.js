f1b029856ae3008d86847d5dd9d72489b921e803
Kyle <kyle.c.r.fahey@gmail.com>
2017-09-06 16:11:38 -0500

    fasfasd

--- client/src/Controller.js
+++ client/src/Controller.js
@@ -6,6 +6,8 @@ import {Row, Col} from 'react-materialize'
 import halfg from './images/half-g.png'
 import schematic from './images/guardian-wire-1.jpg'
 
+import tiles from './images/valve-tiles.jpg'
+
 
 import one from './images/vc-section-1.jpg'
 import two from './images/photo2.png'
@@ -123,201 +125,34 @@ export class Controller extends Component {
 
           </div>
         </Row>
+        <hr />
+        <Row>
+          <Col s={12} className='thirdvalve'>
+            <h2>Battery Backup</h2>
+            <p>The Valve Controller's optional battery backup lets it monitor your system and close the valve for up to twelve hours without electricity</p>
 
- <div className='moreFeaturesContainer controllerAdtlFeat'>
+            <h2>OFFLINE FUNCTIONALITY</h2>
+            <p>Once configured, the internet is only required for off-site control and monitoring of the system, so the Valve Controller will still shut off your water even without the internet during a leak.</p>
 
-                <Row className='moreFeatures'>
-            
-                    <h1 style={{color: 'white', fontWeight: '300', letterSpacing: '1px'}}>Additional Features</h1>
-                    <Col s={12} m={12} l={4}>
-                            <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
+            <h2>Extreme Longrange</h2>
+            <p>The Water Detectors can be placed up to 1000' away from the Valve Controller, which means Guardian will protect large homes and even apartment buildings</p> 
+          </Col>
+        </Row> 
+        <Row>
+          <Col s={12} className='valveTiles'>
+            <img src={tiles} alt='yes' />
 
-                                <div className='featuresInline'>
-                                    <h2>Extreme Long Range</h2>
-                                    <p>Water Detectors can be placed up to 1000' away from the Valve Controller, that's more that an entire city block.</p>
-                                </div>
-                                    
-                            </div>
+          </Col>
 
-                    </Col>
-
-
-                    <Col s={12} m={12} l={4}>
-                        <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
-
-                                <div className='featuresInline'>
-                                <h2>Offline Functionality</h2>
-                                <p>The internet is only required for off-site control and notifications, so Guardian will protect your home without internet connectivity.</p>
-                         </div>
-
-                    </div>
-                    </Col>
-
-
-
-                    <Col s={12} m={12} l={4}>
-                      <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
-
-                                <div className='featuresInline'>
-                                <h2>Battery Backup</h2>
-                                <p>An optional battery backup powers the Valve Controller for an extra twelve hours to protect your home during power outages.</p>
-                        </div>
-                    </div>
-                    </Col>
-                    
-                    
-            
-                </Row>
-
-
-
-                <Row className='moreFeatures two'>
-                    
-                    <Col s={12} m={12} l={4}>
-                        <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
-
-                                <div className='featuresInline'>
-                                <h2>No Tools Required</h2>
-                                <p>Guardian install without any tools or professional help - configuration usually takes under ten minutes.</p>
-                          </div>
-                        </div>
-                    </Col>
-
-                     <Col s={12} m={12} l={4}>
-                       <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
-
-                                <div className='featuresInline'>
-                                <h2>Completely Standalone</h2>
-                                <p>The Water Detectors communicate with directly with the Valve Controller, avoiding the need for hubs or other useless hardware.</p>
-                       </div>
-                    </div>
-                    </Col>
-
-                    <Col s={12} m={12} l={4}>
-                     <div className='featuresCard'>
-               
-                                <img src='http://placehold.it/120x120' alt='Logo' /> 
-
-                                <div className='featuresInline'>
-                                <h2>Routine Self Tests </h2>
-                                <p>The Valve Controller will regularly check your valve to make sure it's not stuck during an emergency.</p>
-                           </div>
-                        </div>
-                    </Col>
-
-                </Row>
-            
-
-            </div>
-            <Row className='specs'> 
-
-                  <div>
-                    <h4>Technical Data</h4>
-                    <table>
-                      <thead>
-                          <tr>
-                            <th style={{textAlign: 'right'}}>&nbsp;</th>
-                            <th>&nbsp;</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Power supply</td>
-                            <td>2x CR123 3.0V battery </td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Working current</td>
-                            <td>35mA</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Power Consumption</td>
-                            <td>2W</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Operating temperature</td>
-                            <td>32 - 104 °F (0 - 40 °C)</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Mounting Options</td>
-                            <td>Wall, tabletop, shelftop</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Warranty</td>
-                            <td>One Year Parts &amp; Labor</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Sensor Dimensions</td>
-                            <td>Ø2.75” x 1.2”</td>
-                          </tr>
-                      </tbody>
-                    </table>
-              
-                </div> 
-                <div>
-                    <h4>Z-Wave Info</h4>
-                    <table>
-                      <thead>
-                          <tr>
-                            <th style={{textAlign: 'right'}}>&nbsp;</th>
-                            <th>&nbsp;</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Radio protocol</td>
-                            <td>Z-Wave(500 series)</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Radio frequency</td>
-                            <td>908.4 MHz US</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Capabilities</td>
-                            <td>Binary Switch (Siren)<br />Indicator (Chimes)<br />Notifications (Siren) <br />Controllable Volumes, Tones, &amp; LEDs</td>
-                          </tr>
-                          <tr>
-                            <td style={{textAlign: 'right'}}>Range</td>
-                            <td>Up to 150’ depending on environment</td>
-                          </tr>
-                      </tbody>
-                    </table>
-                    </div>
-                  <div>
-                    <p style={{display:'none'}}></p>
-                    <h4>Resources</h4>
-                    <p style={{display:'none'}}></p>
-                    <ul>
-                      <li>User Manual (<a href="https://s3-us-west-2.amazonaws.com/dome-manuals/Manuals/Dome+Siren+Manual.pdf" target="_blank">PDF</a>)</li>
-                      <li>Advanced Manual ( <a href="http://docs.domeha.com/#siren" target="_blank">Web</a>)</li>
-                      <li>Installation (<a href="http://docs.domeha.com/#physical-installation16" target="_blank">Web</a>)</li>
-                      <li>Basic Z-Wave Usage (<a href="http://docs.domeha.com/#inclusion-amp-exclusion14" target="_blank">Web</a>)</li>
-                      <li>Command Classes (<a href="http://docs.domeha.com/#compatible-command-classes19" target="_blank">Web</a>)</li>
-                      <li>Configuration Parameters (<a href="http://docs.domeha.com/#configuration-command-class-parameters20" target="_blank">Web</a>)</li>
-                    </ul>
-                    <p style={{display:'none'}}></p>
-                    <h4>Package Contents</h4>
-                    <p style={{display:'none'}}></p>
-                    <ul>
-                      <li>User Manual</li>
-                      <li>Siren</li>
-                      <li>2x Batteries</li>
-                      <li>1x Screw</li>
-                      <li>1x Wall Anchor</li>
-                    </ul>
-                    </div>
-
-              </Row>
-
+        </Row>
+        <Row>
+            <Col s={12}>
+                <video className='purchase' ref='video' poster='http://www.cityrider.com/fixed/43aspect.png' onClick={this.playvideo}>
+                    <source src='https://s3.us-east-2.amazonaws.com/dome-web-assets/purple-passion.mp4' type='video/mp4' />
+                    Your browser does not support HTML5
+                </video>
+            </Col>
+        </Row>
           
          
       </div>   
